62fdc4f6bcd29f7d0060bb22720872babc094980
Atiwat Seenark <atiwat@aquilastudio.net>
2023-01-08 14:27:30 +0700

    feat: add tsx comment plugin and set it up

--- plugins/init.lua
+++ plugins/init.lua
@@ -36,6 +36,26 @@ return {
   ["lukas-reineke/indent-blankline.nvim"] = {
     override_options = indentBlankline
   },
+  ["numToStr/Comment.nvim"] = {
+    override_options = {
+      pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
+      -- pre_hook = function(ctx)
+      --   local U = require "Comment.utils"
+      --
+      --   local location = nil
+      --   if ctx.ctype == U.ctype.block then
+      --     location = require("ts_context_commentstring.utils").get_cursor_location()
+      --   elseif ctx.cmotion == U.cmotion.v or ctx.cmotion == U.cmotion.V then
+      --     location = require("ts_context_commentstring.utils").get_visual_start_location()
+      --   end
+      --
+      --   return require("ts_context_commentstring.internal").calculate_commentstring {
+      --     key = ctx.ctype == U.ctype.line and "__default" or "__multiline",
+      --     location = location,
+      --   }
+      -- end,
+    },
+  },
   ---------------------------------
   -- install
   ---------------------------------
@@ -63,4 +83,5 @@ return {
     end,
   },
   ["p00f/nvim-ts-rainbow"] = {},
+  ["JoosepAlviste/nvim-ts-context-commentstring"] = {}
 }
